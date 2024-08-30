8f062e12251a3ab61cb9eca1cb994cdb9c2c7203
jiaakmal <160829841+jiaakmal@users.noreply.github.com>
2024-08-30 10:44:13 +0500

    COMPLETED PlayeNames dynamic component using state

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,29 +1,38 @@
 import { useState } from "react";
 
-export default function Player({name,symbol}){
+export default function Player({initialName,symbol}){
 
-    const [isEditing , setIsEditing]=useState(false)
+    const [isEditing , setIsEditing]=useState(false);
+    const [playerName,setPlayerName]=useState(initialName);
 
-const clickHandler=()=>{
-    setIsEditing(true);
 
-}  
-let playerName=<span className="player-name">{name}</span>
+function clickHandler(){
+    setIsEditing( (editing)=> !editing );
+}
+
+function handleChange(event){
+    setPlayerName(event.target.value);
+}
+
+  
+let editablePlayerName=<span className="player-name">{playerName}</span>
 
  if(isEditing){
-    return(
-        playerName= <input type="text"  required/>
+  
+        editablePlayerName= <input type="text"  required value={playerName} onChange={handleChange} />
  
-    );
+
  }
 
    
     return(
 <li>
-          {playerName}
+    <span className="player">
+          {editablePlayerName}
           <span className="player-symbol">{symbol}</span>
-         <button onClick={clickHandler}>{isEditing ? 'save'  : 'edit'}</button>
-        </li>
+     </span>
+     <button onClick={clickHandler}>{isEditing?'save':'edit'}</button>
+ </li>
     );
+}
 
-   }
